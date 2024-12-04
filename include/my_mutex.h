aa1aeb29fe6273533721ccb9f7d8e37a1e863384
NotAJocke <jockerider199@gmail.com>
2024-12-05 00:33:53 +0100

    Add semaphore implementation

--- include/my_mutex.h
+++ include/my_mutex.h
@@ -9,4 +9,14 @@ void my_mutex_init(my_mutex_t *mutex);
 void lock(my_mutex_t *mutex);
 void unlock(my_mutex_t *mutex);
 
+typedef struct {
+  volatile int count;
+  my_mutex_t mutex;
+} my_semaphore_t;
+
+void sem_init(my_semaphore_t *sem, int initial_count);
+void sem_wait(my_semaphore_t *sem);
+void sem_post(my_semaphore_t *sem);
+void sem_destroy(my_semaphore_t *sem);
+
 #endif // !MY_MUTEX
